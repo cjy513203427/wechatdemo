a5378dd216850923891da7f37af98a5f76f8a710
cjy513203427 <634208959@qq.com>
2018-04-05 18:26:06 +0800

    Area还原

--- src/main/java/com/imooc/entity/Area.java
+++ src/main/java/com/imooc/entity/Area.java
@@ -56,14 +56,4 @@ public class Area {
         this.lastEditTime = lastEditTime;
     }
 
-    @Override
-    public String toString() {
-        return "Area{" +
-                "areaId=" + areaId +
-                ", areaName='" + areaName + '\'' +
-                ", priority=" + priority +
-                ", createTime=" + createTime +
-                ", lastEditTime=" + lastEditTime +
-                '}';
-    }
 }
